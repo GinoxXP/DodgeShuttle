5f224b4d961c1a7bf7713c8ffe14f65ab035d861
GinoxXP <ginoxxp@yandex.ru>
2020-11-29 13:00:28 +0300

    Added: main menu, exit menu

---
+++ Assets/Scripts/UI/BackToGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToGame : MonoBehaviour
+{
+    public GameObject exitPanel;
+
+    public void Back()
+    {
+        exitPanel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UI/ExitMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ExitMenu : MonoBehaviour
+{
+    public GameObject exitPanel;
+
+    public void OnExit(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+            exitPanel.SetActive(!exitPanel.activeSelf);
+    }
+}
